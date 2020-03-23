bd54f026a6031784fad1c2fe1a05cfdbb9e57aeb
canh3008 <tranduccanh3008@gmail.com>
2020-03-23 10:04:33 +0700

    Random

--- app/src/main/java/com/canh/intent/MainActivity.kt
+++ app/src/main/java/com/canh/intent/MainActivity.kt
@@ -7,14 +7,21 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.function.IntToDoubleFunction
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    btn_first.setOnClickListener {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://howkteam.com"))
-        startActivity(intent)
+        btn_first.setOnClickListener {
+            var min = tv_random1.text.toString()
+            var max = tv_random2.text.toString()
+            var a = Random.nextInt(min.toInt(),max.toInt())
+            tv_random3.text= a.toString()
+        }
+//    btn_first.setOnClickListener {
+//        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://howkteam.com"))
+//        startActivity(intent)
 //        val intent = Intent(this, SecondScreen::class.java)
 //        startActivity(intent)
 
@@ -24,4 +31,3 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-}
